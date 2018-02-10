3684a89726f957258ea9f53245c35779c2bab3ec
Pedro Albuquerque <pedro_m_albuquerque@hotmail.com>
2018-02-10 22:35:16 +0000

    1st attempt - failed

--- examples/graphicstest/graphicstest.ino
+++ examples/graphicstest/graphicstest.ino
@@ -28,21 +28,21 @@ as well as Adafruit raw 1.8" TFT display
 
 // For the breakout, you can use any 2 or 3 pins
 // These pins will also work for the 1.8" TFT shield
-#define TFT_CS     10
-#define TFT_RST    9  // you can also connect this to the Arduino reset
+#define TFT_CS     13 //10
+#define TFT_RST    26 //9  // you can also connect this to the Arduino reset
                       // in which case, set this #define pin to -1!
-#define TFT_DC     8
+#define TFT_DC     27 //8
 
 // Option 1 (recommended): must use the hardware SPI pins
 // (for UNO thats sclk = 13 and sid = 11) and pin 10 must be
 // an output. This is much faster - also required if you want
 // to use the microSD card (see the image drawing example)
-Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
+//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS,  TFT_DC, TFT_RST);
 
 // Option 2: use any pins but a little slower!
-#define TFT_SCLK 13   // set these to be whatever pins you like!
-#define TFT_MOSI 11   // set these to be whatever pins you like!
-//Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+#define TFT_SCLK 12 //13   // set these to be whatever pins you like!
+#define TFT_MOSI 14 //11   // set these to be whatever pins you like!
+Adafruit_ST7735 tft = Adafruit_ST7735(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
 
 
 float p = 3.1415926;
